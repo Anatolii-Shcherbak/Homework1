4978b4851a0653d6aff3ff0a1c9c5fdf155997b8
Anatolii Shcherbak 63570 <ashcher1@stu.vistula.edu.pl>
2023-10-12 20:45:52 +0200

    Homework

---
+++ src/Homework1.java
@@ -0,0 +1,12 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        int y = 1;
+        for(int x = 1; x<100; x++)
+        {
+            y++;
+            System.out.println(y);
+            if(y > 5) { break; }
+        }
+        System.out.println("Hello Vistula");
+    }
+}
